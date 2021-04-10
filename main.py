a9223c889592fd91dedd8a8aa0a4974db42b9e2e
abhishekir <abhishek.ir3@gmail.com>
2021-04-10 10:47:14 -0700

    abstracted game handler

--- main.py
+++ main.py
@@ -242,37 +242,77 @@ class Player:
         screen.blit(PLAYER_IMG, self.get_pos().get_tuple())
 
 
+# manages the repeating logic of the game
+# - collision checking, enemy spawning, removing
+class Game_Handler:
+    def __init__(self):
+        self.player = Player()
+        self.enemyList = [Enemy()]
+        self.last_enemy_spawn_time = 0
+        self.enemy_spawn_rate = 100 # milliseconds between new enemy spawn
 
-def check_collision(object1, object1_width, object1_height,
-                    object2, object2_width, object2_height):
-    if (object1.get_pos().point_dist(object2.get_pos()) < (object1_height + object2_height)): # initial collision check
-        # print("passed near collision check")
-        if ((object1.get_pos().get_x() < object2.get_pos().get_x() + object2_width
-            and object1.get_pos().get_x() + object1_width > object2.get_pos().get_x())
-            and
-            (object1.get_pos().get_y() < object2.get_pos().get_y() + object2_height
-            and object1.get_pos().get_y() + object1_height > object2.get_pos().get_y())):
-            # print("passed actual collision check")
-            # print("Object1 x: %d, y: %d\nObject2 x: %d, y: %d"
-            #       % (object1.get_pos().get_x(), object1.get_pos().get_y(),
-            #       object2.get_pos().get_x(), object2.get_pos().get_y()))
+    def get_player(self):
+        return self.player
 
-            object1.set_collided()
-            object2.set_collided()
-            return True
+    def get_enemyList(self):
+        return self.enemyList
+
+    def main_handler(self):
+        self.get_player().move_player()
+        self.get_player().draw()
+        # handles managing and drawing player bullets
+        self.get_player().handle_bullets()
+
+        # handles removing collided enemies
+        # handles moving, drawing existing enemies
+        self.enemy_handler()
+
+        # handles setting collided flag for enemies with bullets
+        self.collision_handler()
+
+    def check_collision(self, object1, object1_width, object1_height,
+                        object2, object2_width, object2_height):
+        if (object1.get_pos().point_dist(object2.get_pos()) < (object1_height + object2_height)): # initial collision check
+            # print("passed near collision check")
+            if ((object1.get_pos().get_x() < object2.get_pos().get_x() + object2_width
+                and object1.get_pos().get_x() + object1_width > object2.get_pos().get_x())
+                and
+                (object1.get_pos().get_y() < object2.get_pos().get_y() + object2_height
+                and object1.get_pos().get_y() + object1_height > object2.get_pos().get_y())):
+                # print("passed actual collision check")
+                # print("Object1 x: %d, y: %d\nObject2 x: %d, y: %d"
+                #       % (object1.get_pos().get_x(), object1.get_pos().get_y(),
+                #       object2.get_pos().get_x(), object2.get_pos().get_y()))
+
+                object1.set_collided()
+                object2.set_collided()
+                return True
+            else:
+                # print("failed close check")
+                return False
         else:
-            # print("failed close check")
+            # print("failed initial check")
             return False
-    else:
-        # print("failed initial check")
-        return False
+
+    def enemy_handler(self):
+        for enemy in self.get_enemyList():
+            if enemy.get_collision():
+                self.get_enemyList().remove(enemy)
+            else:
+                enemy.move_enemy()
+                enemy.draw()
+
+    def collision_handler(self):
+        for bullet in self.get_player().get_bullet_list():
+            for enemy in self.get_enemyList():
+                self.check_collision(bullet, BULLET_IMG_WIDTH, BULLET_IMG_HEIGHT,
+                                     enemy, ENEMY_IMG_WIDTH, ENEMY_IMG_HEIGHT)
 
 
 def main(screen):
     # Game Loop
     running = True
-    player = Player()
-    enemyList = [Enemy()]
+    handler = Game_Handler()
 
     while running:
         # screen.fill(BACKGROUND_COLOR)
@@ -286,55 +326,31 @@ def main(screen):
                 if event.key == pygame.K_ESCAPE:
                     running = False
                 if event.key == pygame.K_LEFT:
-                    # print("left key down")
-                    player.toggle_left()
+                    handler.get_player().toggle_left()
                 if event.key == pygame.K_RIGHT:
-                    # print("right key down")
-                    player.toggle_right()
+                    handler.get_player().toggle_right()
                 if event.key == pygame.K_UP:
-                    # print("up key down")
-                    player.toggle_up()
+                    handler.get_player().toggle_up()
                 if event.key == pygame.K_DOWN:
-                    # print("down key down")
-                    player.toggle_down()
+                    handler.get_player().toggle_down()
                 if event.key == pygame.K_SPACE:
-                    # print("space bar down")
-                    # player.add_bullet()
-                    player.toggle_firing()
+                    handler.get_player().toggle_firing()
 
             if event.type == pygame.KEYUP:
-                # print("key up")
                 if event.key == pygame.K_LEFT:
-                    player.toggle_left()
+                    handler.get_player().toggle_left()
                 if event.key == pygame.K_RIGHT:
-                    player.toggle_right()
+                    handler.get_player().toggle_right()
                 if event.key == pygame.K_UP:
-                    player.toggle_up()
+                    handler.get_player().toggle_up()
                 if event.key == pygame.K_DOWN:
-                    player.toggle_down()
+                    handler.get_player().toggle_down()
                 if event.key == pygame.K_SPACE:
-                    # print("space bar down")
-                    # player.add_bullet()
-                    player.toggle_firing()
+                    handler.get_player().toggle_firing()
 
         # Game Loop Logic Handler
         if running:
-            player.move_player()
-            player.draw()
-            player.handle_bullets() # handles managing and drawing player bullets
-
-            for enemy in enemyList:
-                if enemy.get_collision():
-                    enemyList.remove(enemy)
-                else:
-                    enemy.move_enemy()
-                    enemy.draw()
-
-            for bullet in player.get_bullet_list():
-                for enemy in enemyList:
-                    check_collision(bullet, BULLET_IMG_WIDTH, BULLET_IMG_HEIGHT,
-                                    enemy, ENEMY_IMG_WIDTH, ENEMY_IMG_HEIGHT)
-
+            handler.main_handler()
             pygame.display.update()
         else:
             pygame.quit()
